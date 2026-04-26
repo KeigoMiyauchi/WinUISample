53ff1038be184977b15f1df4cf11399186cc71a9
keigo_miyauchi <keigo7@outlook.com>
2026-04-26 22:26:32 +0900

    Rename WInUISample to WinUISample

--- WInUISample/MainWindow.xaml.cs
+++ WInUISample/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-namespace WInUISample
+namespace WinUISample
 {
     public sealed partial class MainWindow : Window
     {
